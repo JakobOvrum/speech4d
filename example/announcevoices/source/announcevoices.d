0516250eacaf955060993ff4c4cae7a04f17f4c1
Jakob Øvrum <jakobovrum@gmail.com>
2015-09-11 00:38:39 +0900

    Add example subpackage

--- example/announcevoices/source/announcevoices.d
+++ example/announcevoices/source/announcevoices.d
@@ -1,20 +1,20 @@
 import std.stdio;
 
-import speech.windows.synthesis;
+import speech.synthesis;
 
-version(speech4d_test) void main()
+void main()
 {
 	auto synth = Synthesizer.create();
 
 	auto voices = voiceList();
-	writefln("# of voices: %s", voices.length);
 
 	int i = 0;
 	foreach(voice; voices)
 	{
 		writefln("#%s: %s", i, voice.name);
-		synth.setVoice(voice);
+		synth.voice = voice;
 		synth.speak("Hello, world!");
 		i++;
 	}
 }
+
